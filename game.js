76c6df3e652421fb3b34e74d3191371460f29c30
Kate Dameron <katerj33@gmail.com>
2018-08-01 19:56:56 -0700

    added truth and lie buttons to guessing game with getElementById responses

--- game.js
+++ game.js
@@ -1,16 +1,29 @@
 
-        function guessingGame () {
-            var rightAnswer = 0;
-            var questionOne = prompt('Truth or Lie: When I was driving down the "loneliest road" in Nevada, I saw a wolverine crossing the road.');
-            if ((questionOne.toLowerCase() == "true") || (questionOne.toLowerCase() == "truth")) {
-                alert("Yes, this totally happened! Even though the last known wolverine sighting in that region was in the 1920s!");
-                rightAnswer++;
-                console.log("correct");
-            } else {
-                alert("Wrong! This totally happened! Even though the last known wolverine sighting in that region was in the 1920s!");
-                console.log("nope, that one's true! :-P");
-            }
-            var questionTwo = prompt('Truth or Lie: Growing up I had 1 dog, 3 cats, 2 ducks, 3 box turtles, an iguana, an anole lizard and some goldfish all at the same time.');
+function rightAnswer (answerId) {
+    var submitAnswer = document.getElementById(answerId);
+    submitAnswer.innerHTML = "Right!";
+    console.log("user clicked truth");
+}
+
+function wrongAnswer (answerId) {
+    var submitAnswer = document.getElementById(answerId);
+    submitAnswer.innerHTML = "Wrong!";
+    console.log("user clicked lie");
+}
+/*var rightAnswer = 0;
+function guessTruth () {
+    var submitTruth = "";
+    if ((submitTruth.toLowerCase() == "true") || (submitTruth.toLowerCase() == "truth")) {
+        var submitTruth = document.getElementById("wolverineQ" || "soManyPetsQ");
+        submitTruth.innerHTML = "Right!";
+        console.log("user input truth or true = right answer");
+        rightAnswer++                
+    } else {
+        submitTruth.innerHTML = "Wrong!";
+        console.log("user input lie, no or false = wrong answer");
+    }
+}
+              var questionTwo = prompt('Truth or Lie: Growing up I had 1 dog, 3 cats, 2 ducks, 3 box turtles, an iguana, an anole lizard and some goldfish all at the same time.');
             if ((questionTwo.toLowerCase() == "true") || (questionTwo.toLowerCase() == "truth")) {
                 alert("Ding-ding-ding-ding! Yep, I'm definitely an animal lover");
                 rightAnswer++;
@@ -19,8 +32,8 @@
                 alert("Oops, wrong! I am definitely an animal lover")
                 console.log("nope, that one's true! :-P");
             }
-            var questionThree = prompt('Truth or Lie: I used to be a competative skier when I was younger and almost qualified for the Olympics.');
-            if ((questionThree.toLowerCase() == "false") || (questionThree.toLowerCase() == "lie")) {
+            var quessLie = ""
+            if ((guessLie.toLowerCase() == "false") || (guessLie.toLowerCase() == "lie")) {
                 alert("You're right, I'm not a natural athlete but I did learn to ski about 5 years ago and I love it!");
                 rightAnswer++;
                 console.log("correct");
@@ -28,21 +41,23 @@
                 alert("HAHA, I wish! Naw I learned to ski as an adult.")
                 console.log("nope, that one's false! I learned to ski as an adult :-P");
             } 
-            alert("You got " + rightAnswer + " out of 3.")
-        }
+            alert("You got " + rightAnswer + " out of 3.")*/
+
+
+
         
     
-        function guessTheNum() {
-            var randomNum = Math.floor(Math.random() * 10) + 1;
-            var guessNum = prompt('Now I am going to think of a random number between 1-10. Can you guess what it is?');
-            while (guessNum != randomNum) {
-                if (guessNum > randomNum) {
-                    guessNum = prompt('That number is too high. Try again!');
-                    console.log("That number is too high. Try again!");
-                } else if (guessNum < randomNum) {
-                    guessNum = prompt("That number is too low. Try again!");
-                }
-            }
-            alert("Sweet, good guess!");
-            console.log("Sweet, good guess!");
-            }
+function guessTheNum() {
+    var randomNum = Math.floor(Math.random() * 10) + 1;
+    var guessNum = prompt('Now I am going to think of a random number between 1-10. Can you guess what it is?');
+    while (guessNum != randomNum) {
+        if (guessNum > randomNum) {
+            guessNum = prompt('That number is too high. Try again!');
+            console.log("That number is too high. Try again!");
+        } else if (guessNum < randomNum) {
+            guessNum = prompt("That number is too low. Try again!");
+        }
+    }
+    alert("Sweet, good guess!");
+    console.log("Sweet, good guess!");
+}

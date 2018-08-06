e74667f99bb33e8eac07c2572a5401a74da799a9
Kate Dameron <katerj33@gmail.com>
2018-08-05 17:24:10 -0700

    added some emojis

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
 function rightAnswer (answerId) {
     var submitAnswer = document.getElementById(answerId);
     submitAnswer.innerHTML = "Right!";
+    submitAnswer.innerHTML += "<img src='ebcd691b-1dcd-4d1a-9e1d-1dd36a1c2548.svg'>";
+    submitAnswer.setAttribute("class", "response right");
     console.log("user clicked truth");
 }
 
 function wrongAnswer (answerId) {
     var submitAnswer = document.getElementById(answerId);
     submitAnswer.innerHTML = "Wrong!";
+    submitAnswer.innerHTML += "<img src='7456c25c-bd3a-4bf7-84d9-3f0f5acfc457.svg'>";
+    submitAnswer.setAttribute("class", "response");
     console.log("user clicked lie");
 }
 
@@ -22,7 +26,7 @@ function guessTheNum() {
         }
     }
     var submitAnswer = document.getElementById("number");
-    submitAnswer.innerHTML = "Nice guess";
+    submitAnswer.innerHTML = "<img src='ebcd691b-1dcd-4d1a-9e1d-1dd36a1c2548.svg'>";
     console.log("user clicked guessed number");
 }
 
@@ -34,12 +38,13 @@ function guessVeggies() {
         console.log(vegPrompt);
         if (vegPrompt == veggies[i]) {
             var goodGuess = document.getElementById("guessVeg");
-            goodGuess.innerHTML = "Nom nom nom! Good guess, click again to guess more veggies.";
+            goodGuess.innerHTML = "<img src='5bbdb7b1-9096-4b76-a8ef-2af8638da7e9.svg'>";
+            goodGuess.setAttribute("class", "correct");
             console.log("user picked something growing in my garden");
             break;
         } else {
             var goodGuess = document.getElementById("guessVeg");
-            goodGuess.innerHTML = "Not this year. click to try again.";
+            goodGuess.innerHTML += "Not this year. click to try again.";
             console.log("user chose a vegetable that isn't in my garden");
         }
     }
